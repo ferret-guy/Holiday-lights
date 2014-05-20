9475708ee9177322064005e91a70a07ffae94bb8
Mark Omo <startrek1161@yahoo.com>
2014-05-20 12:33:15 -0700

    Added adafruit ir example code
    https://learn.adafruit.com/ir-sensor/reading-ir-commands

--- main/MAin.ino
+++ main/MAin.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_NeoPixel.h>
+#include ircodes.h
 
 #define PIN 6
 
@@ -73,6 +74,8 @@ void setup() {
   uint32_t color4 = strip.Color(255, 0, 255);
   uint32_t color5 = strip.Color(255, 0, 255);
   uint32_t off = strip.Color(0, 0, 0);
+  Serial.begin(9600);
+  Serial.println("Ready to decode IR!");
 }
 
 void loop() {
@@ -86,10 +89,145 @@ void loop() {
   //theaterChase(strip.Color(127, 127, 127), 50); // White
   //theaterChase(strip.Color(127,   0,   0), 50); // Red
   //theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+  Serial.print("Heard ");
+  Serial.print(numberpulses);
+  Serial.println("-pulse long IR signal");
+  if (IRcompare(numberpulses, ApplePlaySignal,sizeof(ApplePlaySignal)/4)) {
+    Serial.println("PLAY");
+  }
+    if (IRcompare(numberpulses, AppleRewindSignal,sizeof(AppleRewindSignal)/4)) {
+    Serial.println("REWIND");
+  }
+    if (IRcompare(numberpulses, AppleForwardSignal,sizeof(AppleForwardSignal)/4)) {
+    Serial.println("FORWARD");
+  }
+  delay(500);
 }
-//simple color scroll
-void scroll (  ) {}
 
+// from adafruit ir decoder example
+// ****************************************************
+boolean IRcompare(int numpulses, int Signal[], int refsize) {
+  int count = min(numpulses,refsize);
+  Serial.print("count set to: ");
+  Serial.println(count);
+  for (int i=0; i< count-1; i++) {
+    int oncode = pulses[i][1] * RESOLUTION / 10;
+    int offcode = pulses[i+1][0] * RESOLUTION / 10;
+    
+#ifdef DEBUG    
+    Serial.print(oncode); // the ON signal we heard
+    Serial.print(" - ");
+    Serial.print(Signal[i*2 + 0]); // the ON signal we want 
+#endif   
+    
+    // check to make sure the error is less than FUZZINESS percent
+    if ( abs(oncode - Signal[i*2 + 0]) <= (Signal[i*2 + 0] * FUZZINESS / 100)) {
+#ifdef DEBUG
+      Serial.print(" (ok)");
+#endif
+    } else {
+#ifdef DEBUG
+      Serial.print(" (x)");
+#endif
+      // we didn't match perfectly, return a false match
+      return false;
+    }
+    
+    
+#ifdef DEBUG
+    Serial.print("  \t"); // tab
+    Serial.print(offcode); // the OFF signal we heard
+    Serial.print(" - ");
+    Serial.print(Signal[i*2 + 1]); // the OFF signal we want 
+#endif    
+    
+    if ( abs(offcode - Signal[i*2 + 1]) <= (Signal[i*2 + 1] * FUZZINESS / 100)) {
+#ifdef DEBUG
+      Serial.print(" (ok)");
+#endif
+    } else {
+#ifdef DEBUG
+      Serial.print(" (x)");
+#endif
+      // we didn't match perfectly, return a false match
+      return false;
+    }
+    
+#ifdef DEBUG
+    Serial.println();
+#endif
+  }
+  // Everything matched!
+  return true;
+}
+
+int listenForIR(void) {
+  currentpulse = 0;
+  
+  while (1) {
+    uint16_t highpulse, lowpulse;  // temporary storage timing
+    highpulse = lowpulse = 0; // start out with no pulse length
+  
+//  while (digitalRead(IRpin)) { // this is too slow!
+    while (IRpin_PIN & (1 << IRpin)) {
+       // pin is still HIGH
+
+       // count off another few microseconds
+       highpulse++;
+       delayMicroseconds(RESOLUTION);
+
+       // If the pulse is too long, we 'timed out' - either nothing
+       // was received or the code is finished, so print what
+       // we've grabbed so far, and then reset
+       
+       // KGO: Added check for end of receive buffer
+       if (((highpulse >= MAXPULSE) && (currentpulse != 0))|| currentpulse == NUMPULSES) {
+         return currentpulse;
+       }
+    }
+    // we didn't time out so lets stash the reading
+    pulses[currentpulse][0] = highpulse;
+  
+    // same as above
+    while (! (IRpin_PIN & _BV(IRpin))) {
+       // pin is still LOW
+       lowpulse++;
+       delayMicroseconds(RESOLUTION);
+        // KGO: Added check for end of receive buffer
+        if (((lowpulse >= MAXPULSE)  && (currentpulse != 0))|| currentpulse == NUMPULSES) {
+         return currentpulse;
+       }
+    }
+    pulses[currentpulse][1] = lowpulse;
+
+    // we read one high-low pulse successfully, continue!
+    currentpulse++;
+  }
+}
+void printpulses(void) {
+  Serial.println("\n\r\n\rReceived: \n\rOFF \tON");
+  for (uint8_t i = 0; i < currentpulse; i++) {
+    Serial.print(pulses[i][0] * RESOLUTION, DEC);
+    Serial.print(" usec, ");
+    Serial.print(pulses[i][1] * RESOLUTION, DEC);
+    Serial.println(" usec");
+  }
+  
+  // print it in a 'array' format
+  Serial.println("int IRsignal[] = {");
+  Serial.println("// ON, OFF (in 10's of microseconds)");
+  for (uint8_t i = 0; i < currentpulse-1; i++) {
+    Serial.print("\t"); // tab
+    Serial.print(pulses[i][1] * RESOLUTION / 10, DEC);
+    Serial.print(", ");
+    Serial.print(pulses[i+1][0] * RESOLUTION / 10, DEC);
+    Serial.println(",");
+  }
+  Serial.print("\t"); // tab
+  Serial.print(pulses[currentpulse-1][1] * RESOLUTION / 10, DEC);
+  Serial.print(", 0};");
+}
+//*********************************************************************************
 //base color section render
 void render (int offset) {
   //*********************
